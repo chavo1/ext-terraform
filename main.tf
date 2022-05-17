8e80d320987f7e62b6aea1ef16f357043365aa03
Chavdar Rakov <chavdar.rakov@gmail.com>
2022-05-17 16:05:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 data "external" "get_ip_addres_using_shell_dev" {
   program = ["python3","scripts/get_ip_port.py"]
   query = {
-    p_env = "dev"
+    p_env = "qa"
   }
 }
 
-output "ip_address_for_dev" {
+output "ip_address_for_qa" {
   value = data.external.get_ip_addres_using_shell_dev.result.ip_address
 }
 
-output "port_num_for_dev" {
+output "port_num_for_qa" {
   value = data.external.get_ip_addres_using_shell_dev.result.port_num
 }
